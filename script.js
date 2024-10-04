b942ebff1e3f33f299f7b48db4637593222c7b6f
webappbyme <lzaibsiddiqui@gmail.com>
2024-10-04 12:53:22 +0530

    added index.html

---
+++ script.js
@@ -0,0 +1,8 @@
+// Smooth Scrolling between sections
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const section = document.querySelector(this.getAttribute('href'));
+        section.scrollIntoView({ behavior: 'smooth' });
+    });
+});
